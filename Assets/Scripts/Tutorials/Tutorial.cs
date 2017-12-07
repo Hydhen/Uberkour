0941198329505e0273773c45cc35441753f88d1f
Loïc Juillet <juillet.loic@gmail.com>
2017-12-07 21:06:01 +0100

    New prefab + template tutorial for level 1

---
+++ Assets/Scripts/Tutorials/Tutorial.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Tutorial : MonoBehaviour {
+
+    [Tooltip("Title of the Tutorial")]
+    public Text Title = null;
+
+    [Tooltip("Content of the Tutorial")]
+    public Text Content = null;
+
+
+    public void Hide()
+    {
+        if (Title)
+        {
+            Title.gameObject.SetActive(false);
+        }
+
+        if (Content)
+        {
+            Content.gameObject.SetActive(false);
+        }
+    }
+
+    public void Show()
+    {
+        if (Title)
+        {
+            Title.gameObject.SetActive(true);
+        }
+
+        if (Content)
+        {
+            Content.gameObject.SetActive(true);
+        }
+    }
+
+    public void SetTitle(string newTitle)
+    {
+        if (Title)
+        {
+            Title.text = newTitle;
+        }
+    }
+
+    public void SetContent(string newContent)
+    {
+        if (Content)
+        {
+            Content.text = newContent;
+        }
+    }
+}
---
+++ Assets/Scripts/Tutorials/Tutorials.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Tutorials : MonoBehaviour {
+
+    [Tooltip("The whole Tutorial GUI Object")]
+    public GameObject TutorialGameObject = null;
+
+    [Tooltip("The Button that pass to the next Tutorial")]
+    public Button NextButton = null;
+
+    [Tooltip("List of Tutorials to show")]
+    public Tutorial[] Tutos = null;
+
+
+    private int Index = 0;
+
+    private float LastTimeScale = 0f;
+
+    private Text NextButtonText = null;
+
+
+    public void OnNextButtonPressed()
+    {
+        Tutos[Index++].Hide();
+
+        if (Index < Tutos.Length)
+        {
+            if (Index == Tutos.Length - 1 && NextButtonText)
+            {
+                NextButtonText.text = "Terminer";
+            }
+
+            Tutos[Index].Show();
+        }
+        else
+        {
+            NextButton.gameObject.SetActive(false);
+            TutorialGameObject.SetActive(false);
+            Time.timeScale = LastTimeScale;
+        }
+    }
+
+
+    private void Awake()
+    {
+        if (TutorialGameObject == null)
+        {
+            Debug.LogError("<color='Red'>No Tutorial GameObject given</color>");
+        }
+
+        if (NextButton == null)
+        {
+            Debug.LogError("<color='Red'>No Next Button given</color>");
+        }
+        else 
+        {
+            NextButtonText = NextButton.GetComponentInChildren<Text>();
+
+            if (NextButtonText == null)
+            {
+                Debug.LogError("<color='Red'>No Next Button Text found</color>");
+            }
+        }
+
+        if (Tutos.Length > 0)
+        {
+            for (int i = 0; i < Tutos.Length; i++)
+            {
+                Tutos[i].Hide();
+            }
+        }
+    }
+
+    private void Start()
+    {
+        if (Tutos.Length != 0)
+        {
+            LastTimeScale = Time.timeScale;
+            Time.timeScale = 0;
+
+            if (Tutos.Length == 1 && NextButtonText)
+            {
+                NextButtonText.text = "Terminer";
+            }
+
+            Tutos[Index].Show();
+        }
+    }
+
+}
