df9c2a91e37f36f2b3c78a2b5a02e840ef5ff060
Loïc Juillet <juillet.loic@gmail.com>
2017-12-07 21:14:47 +0100

    Add feature to disable gameobject before tutorial

--- Assets/Scripts/Tutorials/Tutorials.cs
+++ Assets/Scripts/Tutorials/Tutorials.cs
@@ -11,6 +11,9 @@ public class Tutorials : MonoBehaviour {
     [Tooltip("The Button that pass to the next Tutorial")]
     public Button NextButton = null;
 
+    [Tooltip("Stuff to disable during the Tutorial, will be re-enable after")]
+    public GameObject[] ObjectToDisable = null;
+
     [Tooltip("List of Tutorials to show")]
     public Tutorial[] Tutos = null;
 
@@ -40,6 +43,14 @@ public class Tutorials : MonoBehaviour {
             NextButton.gameObject.SetActive(false);
             TutorialGameObject.SetActive(false);
             Time.timeScale = LastTimeScale;
+
+            if (ObjectToDisable.Length != 0)
+            {
+                for (int i = 0; i < ObjectToDisable.Length; i++)
+                {
+                    ObjectToDisable[i].SetActive(true);
+                }
+            }
         }
     }
 
@@ -87,6 +98,14 @@ public class Tutorials : MonoBehaviour {
             }
 
             Tutos[Index].Show();
+        }
+
+        if (ObjectToDisable.Length != 0)
+        {
+            for (int i = 0; i < ObjectToDisable.Length; i++)
+            {
+                ObjectToDisable[i].SetActive(false);
+            }
         }
     }
 
