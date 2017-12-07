9132952aae264da815685d07765df8454906d522
Loïc Juillet <juillet.loic@gmail.com>
2017-12-07 12:30:28 +0100

    Add simple menu and loading level system

---
+++ Assets/Scripts/Main Menu/MainMenu.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    [Tooltip("Menu selection")]
+    public GameObject Main = null;
+
+    [Tooltip("Level selection")]
+    public GameObject Levels = null;
+
+
+    #region Public Methods
+
+    public void OnPlayButtonPressed()
+    {
+        Main.SetActive(false);
+        Levels.SetActive(true);
+    }
+
+    public void OnBackButtonPressed()
+    {
+        Levels.SetActive(false);
+        Main.SetActive(true);
+    }
+
+    public void OnLevelButtonPressed()
+    {
+        Debug.Log(EventSystem.current.currentSelectedGameObject.name);
+        SceneManager.LoadScene(EventSystem.current.currentSelectedGameObject.name.ToString());
+    }
+
+    #endregion
+
+
+    #region Private Methods
+
+    private void Awake()
+    {
+        if (Main == null || Levels == null)
+        {
+            Debug.LogError("<color='Red'>No Main menu or Level menu</color>");
+        }
+    }
+
+    private void Start()
+    {
+        if (Levels)
+        {
+            Levels.SetActive(false);
+        }
+    }
+
+    #endregion
+
+}
