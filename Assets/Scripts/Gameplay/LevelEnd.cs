b388dd7be0724dd30de90bad7c8259c825282178
Loïc Juillet <juillet.loic@gmail.com>
2017-12-08 02:23:04 +0100

    Add save system

--- Assets/Scripts/Gameplay/LevelEnd.cs
+++ Assets/Scripts/Gameplay/LevelEnd.cs
@@ -1,19 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelEnd : MonoBehaviour {
 
     [Tooltip("Game Manager")]
-    public MenuInGame MIG;
+    public MenuInGame MIG = null;
+
+
+    private SaveAndLoad SAL = null;
 
 
     public void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
-            MIG.EndLevel();
+            if (MIG)
+            {
+                MIG.EndLevel();
+            }
+
+            if (SAL)
+            {
+                SAL.SetLevelDone(SceneManager.GetActiveScene().buildIndex);
+            }
         }
     }
 
+
+    private void Awake()
+    {
+        SAL = GameObject.FindGameObjectWithTag("SaveAndLoad").GetComponent<SaveAndLoad>();
+    }
 }
---
+++ Assets/Scripts/Utility/SaveAndLoad.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class SaveAndLoad : MonoBehaviour {
+
+    [Tooltip("Number of Level")]
+    public int NumberOfLevels = 2;
+
+
+    private string SaveFileName = "/save.json";
+
+    private GameData Data = null;
+
+
+    public int GetProgression()
+    {
+        return Data.Progression;
+    }
+
+    public void SetLevelDone(int Index)
+    {
+        if (Index > Data.Progression)
+        {
+            Data.Progression = Index;
+        }
+
+        SaveData();
+    }
+
+
+    private bool LoadData()
+    {
+        string filePath = Application.persistentDataPath + SaveFileName;
+
+        if (File.Exists(filePath))
+        {
+            string dataAsJson = File.ReadAllText(filePath);
+            Data = JsonUtility.FromJson<GameData>(dataAsJson);
+
+            Debug.Log("Loaded: " + dataAsJson);
+
+            return true;
+        }
+
+        Data = new GameData();
+        Data.Progression = 1;
+
+        return false;
+    }
+
+    private void SaveData()
+    {
+        string dataAsJson = JsonUtility.ToJson(Data);
+        string filePath = Application.persistentDataPath + SaveFileName;
+
+        File.WriteAllText(filePath, dataAsJson);
+
+        Debug.Log("Saved: " + dataAsJson);
+    }
+
+	private void Awake()
+    {
+        Debug.Log("Save file: " + Application.persistentDataPath + SaveFileName);
+
+        DontDestroyOnLoad(gameObject);
+
+        if (LoadData() == false)
+        {
+            Debug.Log("Save file created");
+            SaveData();
+        }
+        else
+        {
+            Debug.Log("Existing save file used");
+        }
+    }
+}
