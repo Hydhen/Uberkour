5e69a6c46cbcd3f36d46af00cb956512c3ccd1d6
Loïc Juillet <juillet.loic@gmail.com>
2017-12-11 14:32:44 +0100

    Payload Game over

---
+++ Assets/Scripts/Gameplay/Payload.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Payload : MonoBehaviour {
+
+    [Tooltip("Menu handler")]
+    public MenuInGame MIG = null;
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            if (MIG)
+            {
+                MIG.GameOver();
+            }
+            else
+            {
+                Debug.LogError("<color='Red'>No Menu Handler given, cannot loose</color>");
+            }
+        }
+    }
+
+}
